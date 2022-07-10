af20313e5f82217b49a464b264eec3c8310715b9
filipe carvalho <filiperoc@hotmail.com>
2022-07-10 01:04:12 -0300

    styled-components test

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
+import styled, { css } from 'styled-components'
 
 async function getPost(id) {
     const response = await fetch(`http://localhost:3000/json/post-${id}.json`)
@@ -21,15 +22,27 @@ const PostDetails = () => {
     }, [])
 
     return (
-        <section>
+        <Section red>
             <Link to='/'>Voltar para os posts</Link>
             <div>
-                <img src={post.image} alt={post.title} />
+                <Img src={post.image} alt={post.title} />
                 <h2>{post.title}</h2>
                 <p>{post.text}</p>
             </div>
-        </section>
+        </Section>
     )
 }
+
+const Section = styled.section`
+    background-color: blue;
+    ${props => props.red && css `
+        background-color: red;
+    `}
+    padding: 50px;
+`
+
+const Img = styled.img`
+    width: 100%;
+`
 
 export { PostDetails }
